47761ab1c3927c12326faf7e4ab79ccaecdf78e5
ayinanda <ayiniswati22@gmail.com>
2025-09-16 11:07:45 +0700

    Percobaan 3 JS 3 PBO

---
+++ Jobsheet3/Percobaan3/DemoUser12.java
@@ -0,0 +1,9 @@
+package Jobsheet3.Percobaan3;
+
+public class DemoUser12 {
+    public static void main(String[] args) {
+        User12 user1 = new User12("annisa.nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
+
+        user1.cetakInfo();
+    }
+}
